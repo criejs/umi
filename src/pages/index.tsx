41f3005e787c3dc99e56f31e911cde6f4cbf2378
xhf <w_xihengfei@xiwang.com>
2022-08-18 21:39:05 +0800

    优化

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,23 @@
-
 import yayJpg from '@/assets/yay.jpg';
+import Foo from '@/components/Foo';
+import { Helmet } from 'react-helmet';
 
 export default function Page() {
   return (
-    <div>
-      <h2>Yay! Welcome to umi!</h2>
-      <p>
-        <img src={yayJpg} width="388" />
-      </p>
-      <p>
-        To get started, edit <code>pages/index.tsx</code> and save to reload.
-      </p>
-    </div>
+    <>
+      <Helmet>
+        <title>index</title>
+      </Helmet>
+      <div>
+        <Foo />
+        <h2>Yay! Welcome to umi!</h2>
+        <p>
+          <img src={yayJpg} width="388" />
+        </p>
+        <p>
+          To get started, edit <code>pages/index.tsx</code> and save to reload.
+        </p>
+      </div>
+    </>
   );
 }
